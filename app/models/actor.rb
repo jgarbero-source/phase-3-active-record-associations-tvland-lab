1c5ef3c8c7792d67d180c8a432f952439845aa40
John Garbero <john.garbero.itb@gmail.com>
2022-07-05 11:36:50 -0500

    commit tree 3ff7e4b223c94f3d0aa9f127e43b5fc27725818c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    role = ""
+    the_show = ""
+    self.characters.collect do |char|
+        role = char.name
+    end
+    self.shows.collect do |sho|
+        the_show = sho.name
+    end
+    "#{role} - #{the_show}"
+  end
 end
